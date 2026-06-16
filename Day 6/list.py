587377fbaba43dfbe5be861625aaaf29203c6536
ruth sisay <ruth22sisay@gmail.com>
2026-06-16 19:22:36 +0300

    Add list(array) solution

---
+++ Day 6/list.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    
+    my_list = []
+    
+    my_list.insert(0, 5)       
+    my_list.insert(1, 10)      
+    my_list.insert(0, 6)       
+    print(my_list)            
+
+    my_list.remove(6)      
+    my_list.append(9)         
+    my_list.append(1)          
+    my_list.sort()            
+    print(my_list)             
+
+    my_list.pop()              
+    my_list.reverse()         
+    print(my_list)
+
